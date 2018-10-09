7302e4b5de7874c5f70fdc9c425b139c5eb4760e
varun3210 <43983336+varun3210@users.noreply.github.com>
2018-10-09 13:45:11 +0530

    Add files via upload

---
+++ lab_function.py
@@ -0,0 +1,14 @@
+def ArmN(x):
+ sum=0
+ t=x
+ while(t>0):
+  d=t%10
+  sum+=d**3
+  t=t//10
+ if sum==x:
+    return 'Armstrong Number'
+ else:
+    return 'Not Armstrong Number'
+x=int(input('Enter Number'))
+print(ArmN(x)) 
+
